669a05761f329b1c81988af3854f44a284740ae2
piyush-24 <72154693+piyush-24@users.noreply.github.com>
2022-01-11 11:56:33 +0530

    Time: 7 ms (25.71%), Space: 6.6 MB (64.30%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -2,26 +2,11 @@ class Solution {
 public:
     string removeOccurrences(string s, string part)
     {
-         int i = 0, j = 0, k= part.length();
+         int n=s.size();
         
-        while(j<s.length())
-        {
-            if(j-i+1<k) // less than window size
-                j++;
-            else if(j-i+1 == k)                         // equal to window size
-            { 
-                if(s.substr(i,k) == part)
-                {                                       //matched
-                    s.erase(i,k);                       //remove
-                    i = 0; j = 0;                       //reset pointers
-                }
-                else
-                {
-                    i++;
-                    j++; //otherwise incremenet pointers
-                }
-            }
-        }
+        while(n!=0 && s.find(part)<n)
+            s.erase(s.find(part),part.size());
+        
         return s;
     }
 };
