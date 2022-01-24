3085bc9f6836b27dd4ebfd2eecba172c57ac5c99
piyush-24 <72154693+piyush-24@users.noreply.github.com>
2022-01-24 19:28:45 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (9.79%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -2,30 +2,30 @@ class Solution {
 public:
     bool isValid(string s)
     {
-        unordered_map<char,char> m;
-        stack<int> st;
-        m[')']='(';
-        m['}']='{';
-        m[']']='[';
-        
-        if(s.size() &1)
-            return false;
-        
-        for(int i=0;i<s.size();i++)
+         stack<char> st;
+       for(char c : s)
+       {
+        if(c == '('|| c == '{' || c == '[')
         {
-            if(s[i]=='(' || s[i]=='{' || s[i]=='[')
-                st.push(s[i]);
-            else
-            {
-                char x=s[i];
-                if(!st.empty() && st.top()==m[x])
-                    st.pop();
-                else
-                    return false;
-            }
+            st.push(c);
         }
-        if(st.size()>0)
-            return false;
-        return true;
+           else
+           {
+            if(st.empty()) 
+                return false;
+               
+            if(c == ')' && st.top() != '(') 
+                return false;
+               
+            if(c == '}' && st.top() != '{') 
+                return false;
+               
+            if(c == ']' && st.top() != '[') 
+                return false;
+               
+            st.pop();
+        }
+    }
+    return st.empty();
     }
 };
