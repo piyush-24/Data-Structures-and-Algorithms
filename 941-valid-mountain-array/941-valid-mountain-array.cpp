0fa100bd3ccbc0d638cb7f08e8bc7cd032e75bfa
piyush-24 <72154693+piyush-24@users.noreply.github.com>
2022-01-26 02:34:39 +0530

    Time: 28 ms (74.96%), Space: 22.4 MB (48.35%) - LeetHub

--- 941-valid-mountain-array/941-valid-mountain-array.cpp
+++ 941-valid-mountain-array/941-valid-mountain-array.cpp
@@ -1,34 +1,15 @@
 class Solution {
 public:
-    bool validMountainArray(vector<int>& arr) 
+    bool validMountainArray(vector<int>& A)
     {
-        bool mt=false;
-        if(arr.size()<3)
-            return false;
-            
-        int mark=-1;
-        for(int i=1;i<arr.size()-1;i++)
-        {
-            if(arr[i]==arr[i-1])
-                return false;
-            if(arr[i]>arr[i-1] && arr[i]>arr[i+1])
-               mark=i;
-        }
+        int n = A.size(), i = 0, j = n - 1;
         
-        if(mark==-1)
-            return false;
+        while (i + 1 < n && A[i] < A[i + 1]) 
+            i++;
         
-        for(int i=1;i<=mark;i++)
-        {
-            if(arr[i-1]>=arr[i])
-                return false;
-        }
-        for(int i=mark+1;i<arr.size();i++)
-        {
-            if(arr[i-1]<=arr[i])
-                return false;
-        }
+        while (j > 0 && A[j - 1] > A[j])
+            j--;
         
-        return true;
+        return (i > 0 && i == j && j < n - 1);
     }
 };
