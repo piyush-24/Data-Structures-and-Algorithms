ebc42fa50a62b092d62dd3ea8a05e00cc4bf2d02
piyush-24 <72154693+piyush-24@users.noreply.github.com>
2022-01-26 12:25:36 +0530

    Time: 25 ms (22.61%), Space: 9.8 MB (91.77%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -5,9 +5,9 @@ public:
     {
         int n=triangle.size();
         
-        vector<vector<int>> dp(n,vector<int>(n,0));
+        vector<int> front(n,0), curr(n,0);
         for(int i=0;i<n;i++)
-            dp[n-1][i]=triangle[n-1][i];
+            front[i]=triangle[n-1][i];
         
         
         for(int i=n-2;i>=0;i--)
@@ -15,21 +15,21 @@ public:
             for(int j=0;j<n;j++)
             {
                  int rdg=INT_MAX,ldg=INT_MAX,d=INT_MAX;
-                 d=  triangle[i][j]+dp[i+1][j];
+                 d=  triangle[i][j]+front[j];
                 
                 if(j+1<n)
-                 rdg= triangle[i][j]+dp[i+1][j+1];
+                 rdg= triangle[i][j]+front[j+1];
                 
                 if(j-1>=0)
-                 ldg= triangle[i][j]+dp[i+1][j-1];
+                 ldg= triangle[i][j]+front[j-1];
 
-                 dp[i][j]=min({d,rdg,ldg});
+                curr[j]=min({d,rdg,ldg});
             }
-            
+          front=curr;  
         }
         int ans=INT_MAX;
         for(int i=0;i<n;i++)
-            ans=min(ans,dp[0][i]);
+            ans=min(ans,front[i]);
         return ans ;
     }
 };
