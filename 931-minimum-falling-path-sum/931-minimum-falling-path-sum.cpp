24f0d512b0a12bf11e2b8f19ae075e780e9fea07
piyush-24 <72154693+piyush-24@users.noreply.github.com>
2022-01-26 12:17:31 +0530

    Time: 16 ms (57.21%), Space: 10.4 MB (5.84%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -1,31 +1,35 @@
 class Solution {
 public:
-    int f(int i, int j, vector<vector<int>>& triangle, int n, vector<vector<int>>& dp)
-    {
-        if(i==n-1)
-            return triangle[n-1][j];
-        
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        
-        int rdg=INT_MAX,ldg=INT_MAX,d=INT_MAX;
-         d=  triangle[i][j]+f(i+1,j,triangle,n,dp);
-        if(j+1<n)
-         rdg= triangle[i][j]+f(i+1,j+1,triangle,n,dp);
-        if(j-1>=0)
-         ldg= triangle[i][j]+f(i+1,j-1,triangle,n,dp);
-        
-        return dp[i][j]=min({d,rdg,ldg});
-    }
+   
     int minFallingPathSum(vector<vector<int>>& triangle)
     {
         int n=triangle.size();
+        
+        vector<vector<int>> dp(n,vector<int>(n,0));
+        for(int i=0;i<n;i++)
+            dp[n-1][i]=triangle[n-1][i];
+        
+        
+        for(int i=n-2;i>=0;i--)
+        {
+            for(int j=0;j<n;j++)
+            {
+                 int rdg=INT_MAX,ldg=INT_MAX,d=INT_MAX;
+                 d=  triangle[i][j]+dp[i+1][j];
+                
+                if(j+1<n)
+                 rdg= triangle[i][j]+dp[i+1][j+1];
+                
+                if(j-1>=0)
+                 ldg= triangle[i][j]+dp[i+1][j-1];
+
+                 dp[i][j]=min({d,rdg,ldg});
+            }
+            
+        }
         int ans=INT_MAX;
         for(int i=0;i<n;i++)
-        {
-            vector<vector<int>> dp(n,vector<int>(n,-1));
-            ans=min(ans,f(0,i,triangle,n,dp));
-        }
+            ans=min(ans,dp[0][i]);
         return ans ;
     }
 };
