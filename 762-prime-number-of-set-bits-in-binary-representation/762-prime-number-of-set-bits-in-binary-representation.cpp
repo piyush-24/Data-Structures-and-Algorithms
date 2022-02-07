d90d4b6e2cf2b2783ddf5b7774c45699c992538f
piyush-24 <72154693+piyush-24@users.noreply.github.com>
2022-02-07 20:50:25 +0530

    Time: 6 ms (81.80%), Space: 5.9 MB (46.19%) - LeetHub

---
+++ 762-prime-number-of-set-bits-in-binary-representation/762-prime-number-of-set-bits-in-binary-representation.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int countPrimeSetBits(int L, int R) 
+    {
+        int count=0;
+        for(int i=L;i<=R;i++)
+        {
+            if(checkPrime(findSetBits(i))) 
+                count++;
+        }
+        return count;
+    }
+    int findSetBits(int n)
+    {
+        int count=0;
+        while(n)
+        {
+            n=n&(n-1);
+            count++;
+        }
+        return count;
+    }
+    bool checkPrime(int x)
+    {
+        return (x == 2 || x == 3 || x == 5 || x == 7 ||
+                x == 11 || x == 13 || x == 17 || x == 19);
+    }
+};
