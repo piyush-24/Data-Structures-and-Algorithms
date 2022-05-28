30376b5ef5e2aab8845d5ec52e281a062f982942
piyush-24 <72154693+piyush-24@users.noreply.github.com>
2022-05-28 13:58:33 +0530

    Time: 0 ms (100.00%), Space: 6 MB (97.93%) - LeetHub

--- 1844-replace-all-digits-with-characters/1844-replace-all-digits-with-characters.cpp
+++ 1844-replace-all-digits-with-characters/1844-replace-all-digits-with-characters.cpp
@@ -2,8 +2,10 @@ class Solution {
 public:
     string replaceDigits(string s)
     {
-         for (auto i = 1; i < s.size(); i += 2)
-              s[i] += s[i - 1] - '0';
-      return s;
+        for(int i=1;i<s.size();i+=2)
+        {
+            s[i]=s[i-1]+(s[i]-'0'); //or s[i] += s[i-1] - '0';
+        }
+        return s;
     }
 };
