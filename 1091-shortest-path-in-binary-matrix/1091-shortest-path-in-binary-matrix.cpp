144e6d3148cd3c513eaf49488feb18ed4f2cc1b1
piyush-24 <72154693+piyush-24@users.noreply.github.com>
2022-06-10 15:11:37 +0530

    Time: 59 ms (97.06%), Space: 19.2 MB (83.80%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -11,32 +11,37 @@ public:
         if (grid[0][0]==1 || grid[n-1][n-1]) 
             return -1;
         
-        queue<pair<pair<int,int>,int>> q;
-        q.push({{0,0},1});
+        queue<pair<int,int>> q;
+        q.push({0,0});
         grid[0][0] = 1 ;
-        
+        int st=1;
         while(!q.empty()) 
         {
-            int i= q.front().first.first;
-            int j = q.front().first.second;
-            int st = q.front().second;
-            q.pop();
-            if (i == n- 1 && j == n-1)
+            int sz=q.size();
+            while(sz--)
             {
-                return st;
-            }
-            
-            for(int k= 0 ;k <8;k++)
-            {
-                int x = i + dx[k];
-                int y = j +dy[k];
-                
-                if(x>=0 && x <n && y >=0 && y <n && grid[x][y] == 0 ) 
+                int i= q.front().first;
+                int j = q.front().second;
+           
+                q.pop();
+                if (i == n- 1 && j == n-1)
                 {
-                    grid[x][y] = 1;
-                    q.push({{x,y},st+1});
+                    return st;
+                }
+
+                for(int k= 0 ;k <8;k++)
+                {
+                    int x = i + dx[k];
+                    int y = j +dy[k];
+
+                    if(x>=0 && x <n && y >=0 && y <n && grid[x][y] == 0 ) 
+                    {
+                        grid[x][y] = 1;
+                        q.push({x,y});
+                    }
                 }
             }
+          st++;
         }
         return -1;
     }
