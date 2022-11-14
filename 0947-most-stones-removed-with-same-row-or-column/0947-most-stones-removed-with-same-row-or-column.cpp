c4dbbf6d7a1cb7de2461b82bd151601cac4608af
piyush-24 <72154693+piyush-24@users.noreply.github.com>
2022-11-15 00:53:15 +0530

    Time: 323 ms (46.64%), Space: 19.4 MB (39.30%) - LeetHub

---
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -0,0 +1,70 @@
+class Disjoint
+{
+    public:
+    vector<unsigned long long> size,parent;
+    Disjoint(long long n)
+    {
+        size.resize(n+1,1);
+        parent.resize(n+1);
+        
+        for(int i=0;i<n+1;i++)
+            parent[i]=i;
+    }
+    
+    long long findPar(long long n)
+    {
+        if(parent[n]==n)
+            return n;
+        
+        return parent[n]=findPar(parent[n]);
+    }
+    
+    void Union(long long u, long long v)
+    {
+        long long ulp_u=findPar(u);
+        long long ulp_v=findPar(v);
+        
+        if(size[ulp_u] > size[ulp_v])
+        {
+            parent[ulp_v]=ulp_u;
+            size[ulp_u]+=size[ulp_v];
+        }
+        else
+        {
+            parent[ulp_u]=ulp_v;
+            size[ulp_v]+=size[ulp_u];
+        }
+    }
+};
+class Solution {
+public:
+    int removeStones(vector<vector<int>>& stones) {
+        int max_r=0;
+        int max_c=0;
+        
+        for(auto it:stones)
+        {
+            max_r=max(max_r, it[0]);
+            max_c=max(max_c, it[1]);   
+        }
+        
+        Disjoint ds(max_r+max_c+1);
+        
+        for(auto it:stones)
+        {
+            long long r=it[0];
+            long long c=it[1] + max_r + 1;
+            
+            ds.Union(r,c);
+        }
+        long long c=0;
+        for(int i=0;i<=max_r+max_c+1;i++)
+        {
+            cout<<ds.size[i]<<endl;
+            if(ds.size[i]>1 && ds.parent[i]==i)
+                c++;
+        }
+        
+        return (stones.size()-c);
+    }
+};
