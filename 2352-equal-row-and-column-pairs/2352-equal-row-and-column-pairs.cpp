28e429e3f0b191387628aa6779dee37e642cf433
piyush-24 <72154693+piyush-24@users.noreply.github.com>
2023-01-03 11:47:47 +0530

    Time: 604 ms (22.24%), Space: 44.3 MB (13.34%) - LeetHub

---
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int equalPairs(vector<vector<int>>& grid) {
+        
+        int n=grid.size();
+        
+        map<string, int> st;
+        
+        for(int i=0;i<n;i++)
+        {
+            string s="";
+            for(int j=0;j<n;j++)
+            {
+                s+=to_string(grid[i][j])+"#";
+            }
+           st[s]++;
+        }
+        int c=0;
+         for(int i=0;i<n;i++)
+        {
+            string s="";
+            for(int j=0;j<n;j++)
+            {
+                s+=to_string(grid[j][i])+"#";
+            }
+            if(st[s]>0)
+                c+=(st[s]);
+        }
+       return c; 
+    }
+};
