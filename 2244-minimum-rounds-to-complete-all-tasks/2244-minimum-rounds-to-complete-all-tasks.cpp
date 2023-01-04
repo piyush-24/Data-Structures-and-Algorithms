8fdd0762b92cf56d6ea48b8b7817d41d638fd615
piyush-24 <72154693+piyush-24@users.noreply.github.com>
2023-01-04 19:40:50 +0530

    Time: 346 ms (54.96%), Space: 104.6 MB (23.86%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -5,33 +5,18 @@ public:
         for(auto it:tasks)
              m[it]++;
         
-        for(auto it:m)
-        {
-            if(it.second==1)
-                return -1;
-        }
+      
         int ans=0;
          for(auto it:m)
         {
-             cout<<it.first<<" "<<it.second<<endl;
+              if(it.second==1)
+                return -1;
              
             if((it.second%3)==0)
                 ans+=(it.second/3);
-            else if((it.second)==2)
-               ans+=1;
             else
-            {
-                ans++;
-                int x=it.second;
-                while(x>=3)
-                {
-                    ans++;
-                    x-=3;
-                }
-                
-            }
-             
-             cout<<ans<<endl;
+                ans+=(it.second/3 + 1);
+  
         }
         
        return ans; 
